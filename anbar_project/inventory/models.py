d9db065bb17f064e2f60d36026857b16f897527c
amin <biglarymohamadamin75@gmail.com>
2023-10-16 20:10:50 +0330

    add Item model

--- anbar_project/inventory/models.py
+++ anbar_project/inventory/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Item(models.Model):
+    name = models.CharField(max_length=40)
+    number = models.IntegerField()
+    description = models.TextField()
+    picture = models.ImageField()
+    ITEM_STATUS = (
+        ('o', 'operational'),
+        ('n', 'non-operational'),
+    )
+
+    status = models.CharField(
+        max_length=1,
+        choices=ITEM_STATUS,
+        default='o',
+        help_text='Current status of the item',
+    )
+
+    delivery_date = models.DateTimeField(auto_now_add=True, blank=True)
+
+    def __str__(self):
+        return self.name
