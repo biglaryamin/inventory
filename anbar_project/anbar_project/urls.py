42572076194f73a0fe90f2b2dd5d6dda17fd5a65
amin <biglarymohamadamin75@gmail.com>
2023-10-19 11:08:47 +0330

    add urls for api in url file of inventory app

--- anbar_project/anbar_project/urls.py
+++ anbar_project/anbar_project/urls.py
@@ -20,7 +20,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls'))
+    path('', include('inventory.urls')),    
 ]
 
 
