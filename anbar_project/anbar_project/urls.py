26981d70b2af25a33d3892db7d2491220e8200fc
amin <biglarymohamadamin75@gmail.com>
2023-11-19 22:20:22 +0330

    add static volumes in docker-compose

--- anbar_project/anbar_project/urls.py
+++ anbar_project/anbar_project/urls.py
@@ -21,8 +21,7 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('inventory.urls')),    
-]
-
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
