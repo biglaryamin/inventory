5fa338c0689467c4ef8907ef93a847b5850e4c95
amin <biglarymohamadamin75@gmail.com>
2023-12-25 16:25:09 +0330

    delete unsused imports in admin.py file in ainventory app

--- anbar_project/inventory/admin.py
+++ anbar_project/inventory/admin.py
@@ -1,7 +1,4 @@
 from django.contrib import admin
-from django.utils.translation import gettext_lazy as _
-from django.urls import reverse
-from django.http import HttpResponseRedirect
 from .models import Item
 
 from imagekit.admin import AdminThumbnail
